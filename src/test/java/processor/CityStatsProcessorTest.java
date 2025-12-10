b95dcaa73be006cc7d71d2dd9ab5f7aac88d19bf
Alexander Liu <al4402@columbia.edu>
2025-12-09 23:03:39 -0500

    Fixed an error in CityStatsProcessorTest.java

--- src/test/java/processor/CityStatsProcessorTest.java
+++ src/test/java/processor/CityStatsProcessorTest.java
@@ -2,16 +2,18 @@ package processor;
 
 import data.models.ParkingViolation;
 import data.models.PopulationData;
+import processor.cache.StatsCache;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CityStatsProcessorTest {
     @Test
-    public void testViolationsPerCapita() {
+    public void testViolationsPerCapita() throws ExecutionException, InterruptedException {
         PopulationProcessor pop = new PopulationProcessor(
                 List.of(new PopulationData("19104", 2000))
         );
@@ -24,7 +26,10 @@ public class CityStatsProcessorTest {
                 )
         );
 
-        CityStatsProcessor stats = new CityStatsProcessor(pop, housing, parking);
+        StatsCache.initializeCacheBuild(housing, parking, pop);
+        StatsCache cache = StatsCache.getInstance();
+
+        CityStatsProcessor stats = new CityStatsProcessor(pop, housing, parking, cache);
 
         assertEquals(0.001, stats.getViolationsPerCapita("19104"), 0.00001);
         assertEquals(0.0, stats.getViolationsPerCapita("BADZIP"), 0.001);
